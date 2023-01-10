99c9b5e0b26ed5da0cc50cc061658ddf2d24c302
Yaroslavhal <yaroslav.halushko.n@gmail.com>
2023-01-10 19:18:48 +0200

    Added BAL

---
+++ BAL/DTO/Models/UserEntityDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAL.DTO.Models
+{
+    public class UserEntityDTO : BaseEntityDTO<int>
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public virtual ICollection<BasketEntityDTO> Baskets { get; set; }
+        public virtual ICollection<OrderEntityDTO> Orders { get; set; }
+    }
+}
